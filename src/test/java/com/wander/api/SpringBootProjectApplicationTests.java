ee1bda23c22c41f5ffba6fbcff7237fa4adc41e2
Wanderson <wander.semog@gmail.com>
2019-11-10 15:23:44 -0300

    first commit

---
+++ src/test/java/com/wander/api/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
