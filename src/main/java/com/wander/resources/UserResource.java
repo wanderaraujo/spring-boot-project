bdb41a17ead5fc7d317bc0be1a004107033eba68
Wanderson Araujo <wanderson@intricately.com>
2019-11-26 21:48:55 -0300

    Add insert user

--- src/main/java/com/wander/resources/UserResource.java
+++ src/main/java/com/wander/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.wander.domain.User;
 import com.wander.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,5 +32,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findAll(@PathVariable String id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/wander/service/UserService.java
+++ src/main/java/com/wander/service/UserService.java
@@ -1,5 +1,6 @@
 package com.wander.service;
 
+import com.wander.config.UserDTO;
 import com.wander.domain.User;
 import com.wander.exception.ObejectNotFoundException;
 import com.wander.repository.UserRepository;
@@ -25,5 +26,13 @@ public class UserService {
             throw new ObejectNotFoundException("Object not found!");
         }
         return user.get();
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(),objDto.getName(),objDto.getName());
     }
 }
